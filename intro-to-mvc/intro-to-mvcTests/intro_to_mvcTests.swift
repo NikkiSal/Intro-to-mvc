bfb987fda2d160979f274a8e8bf663d839154aae
Nikki <Nikkisali9@gmail.com>
2019-09-18 13:49:21 -0600

    first commit

---
+++ intro-to-mvc/intro-to-mvcTests/intro_to_mvcTests.swift
@@ -0,0 +1,34 @@
+//
+//  intro_to_mvcTests.swift
+//  intro-to-mvcTests
+//
+//  Created by Marzieh on 2019-09-18.
+//  Copyright © 2019 Myph. All rights reserved.
+//
+
+import XCTest
+@testable import intro_to_mvc
+
+class intro_to_mvcTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
